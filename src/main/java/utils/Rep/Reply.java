c668296034da856229fe1b65d3da34b394a6467b
FranciscoFreitas45 <zefreitas2011@hotmail.com>
2019-12-28 18:42:33 +0000

    client

--- src/main/java/utils/Rep/Reply.java
+++
@@ -1,9 +0,0 @@
-package utils.Rep;
-
-import io.atomix.cluster.messaging.ManagedMessagingService;
-import io.atomix.utils.net.Address;
-import io.atomix.utils.serializer.Serializer;
-
-public interface Reply {
-    public void sender(ManagedMessagingService ms, Address address, Serializer s);
-}
